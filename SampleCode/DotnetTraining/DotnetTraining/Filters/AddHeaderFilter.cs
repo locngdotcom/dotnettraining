909af54e0ae29a6378e26c59448d38a92f2c9c49
HARVEYNASH\locnguyenh <nguyenhuuloc304@gmail.com>
2021-10-04 16:06:27 +0700

    update code

---
+++ SampleCode/DotnetTraining/DotnetTraining/Filters/AddHeaderFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetTraining.Filters
+{
+    public class AddHeaderFilter : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.HttpContext.Request.Headers.Add("RookieRequest", "ABC");
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            context.HttpContext.Response.Headers.Add("RookieReturn", "DEF");
+        }
+    }
+}
